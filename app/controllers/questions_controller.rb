49ae0fb8a53cdc89e4a52416e27bcdc96fbc4054
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-02-22 12:14:30 +0100

    stupid coach can answer questions OK

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    # If the message is I am going to work, the coach will answer Great!
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+    elsif @question[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!.'
+    else
+      # Otherwise the coach will answer I don't care, get dressed and go to work!
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
